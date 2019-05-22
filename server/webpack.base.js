b60966e586effca5e5afb2627aa6254e10caba56
Angga Ari Wijaya <anggadarkprince@gmail.com>
2019-05-23 06:47:26 +0700

    boot up react in client

--- server/webpack.base.js
+++ server/webpack.base.js
@@ -10,7 +10,7 @@ module.exports = {
                     options: {
                         presets: [
                             '@babel/preset-react',
-                            //'@babel/preset-stage-0',
+                            //'@babel/plugin-syntax-dynamic-import',
                             '@babel/preset-env'
                         ]
                     }
---
+++ server/webpack.client.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
+
+const config = {
+    mode: 'development',
+
+    // Tell webpack the root file of our
+    // client application
+    entry: './src/client/client.js',
+
+    // Tell webpack where to put the output file
+    // that is generated
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    externals: [webpackNodeExternals()]
+};
+
+module.exports = merge(baseConfig, config);
