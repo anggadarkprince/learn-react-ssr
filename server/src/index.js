b8fd51dbec2e63dc516109a7bbafcdb3d82dbe18
Angga Ari Wijaya <anggadarkprince@gmail.com>
2019-06-10 19:22:51 +0700

    refactor pages

--- server/src/index.js
+++ server/src/index.js
@@ -25,6 +25,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(4000, () => {
+app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
--- server/webpack.client.js
+++ server/webpack.client.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
 
 const config = {
-    mode: 'production',
+    mode: 'development',
 
     // Tell webpack the root file of our
     // client application
